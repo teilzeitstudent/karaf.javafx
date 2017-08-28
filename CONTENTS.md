5a91659b1794f39780989059afadcb2afd25b8cf
teilzeitstudent <teilzeitstudent@googlemail.com>
2017-08-28 04:19:19 +0100

    Basic JavaFX application
    This is not an OSGI bundle yet. Just a basic JavaFX application.
    
    Code taken from
    http://docs.oracle.com/javafx/2/get_started/hello_world.htm

---
+++ CONTENTS.md
@@ -0,0 +1,14 @@
+A quick overview of the contents of this directory
+
+1. **java/maven bundles**
+These are maven bundles. The naming convention is
+	groupid: com.github.teilzeitstudent
+	artifactid: karafjavafx.whatever
+	directory name: groupid.artifactid
+ Leads to kinda long'sh directory names, but whatever.
+ 1.1. **com.github.teilzeitstudent.karafjavafx.main**
+ Basic javafx application that shows a simple dialog box
+1. **misc**
+Anything not fitting into the other categories
+1.1. **karaf**
+ This is a basic karaf installation.
---
+++ com.github.teilzeitstudent.karafjavafx.main/src/main/java/com/github/teilzeitstudent/karafjavafx/main/JavaxExample.java
@@ -0,0 +1,36 @@
+package com.github.teilzeitstudent.karafjavafx.main;
+
+
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+
+public class JavaxExample extends Application {
+	public static void main(String[] args) {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+	}
+
+}
