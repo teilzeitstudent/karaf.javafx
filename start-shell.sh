7cf0500523be81566587e986710adaa8222e7749
teilzeitstudent <teilzeitstudent@googlemail.com>
2017-08-30 18:14:06 +0100

    Get rid of more IGB stuff
    Only really need those 2 scripts, no need to delete stuff.
    Not yet anyway.

--- start-shell.sh
+++
@@ -1,5 +0,0 @@
-#rm -rf karaf/deploy/*
-#rm -rf karaf/local/*
-#mkdir karaf/deploy -p
-#cp org.lorainelab.igb.feature/target/features/features.xml karaf/deploy/
-karaf/bin/igb clean debug
--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-#rm -rf karaf/deploy/*
-#cp org.lorainelab.igb.feature/target/*.kar karaf/deploy/
-karaf/bin/igb server
+karaf/bin/karaf server
