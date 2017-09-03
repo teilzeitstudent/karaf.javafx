ddba532e8cce1154b1cba11125334fc3f8eb3cf3
teilzeitstudent <teilzeitstudent@googlemail.com>
2017-09-03 22:48:29 +0100

    Basic OSGI bundle activator

--- com.github.teilzeitstudent.karafjavafx.main/src/main/java/com/github/teilzeitstudent/karafjavafx/main/JavaxExample.java
+++ com.github.teilzeitstudent.karafjavafx.main/src/main/java/com/github/teilzeitstudent/karafjavafx/main/JavaxExample.java
@@ -1,6 +1,9 @@
 package com.github.teilzeitstudent.karafjavafx.main;
 
 
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
 import javafx.application.Application;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -9,7 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 
-public class JavaxExample extends Application {
+public class JavaxExample extends Application implements BundleActivator {
 	public static void main(String[] args) {
         launch(args);
     }
@@ -31,6 +34,16 @@ public class JavaxExample extends Application {
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Start");
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Stop");
 	}
 
 }
