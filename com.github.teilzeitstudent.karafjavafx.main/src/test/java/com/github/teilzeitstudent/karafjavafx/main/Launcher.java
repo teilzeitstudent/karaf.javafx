0d3ba7cc66d3f176b803f36aa37dd25c45595120
teilzeitstudent <teilzeitstudent@googlemail.com>
2017-09-03 23:08:33 +0100

    Use anonymous greeting services in launcher

--- com.github.teilzeitstudent.karafjavafx.main/src/test/java/com/github/teilzeitstudent/karafjavafx/main/Launcher.java
+++ com.github.teilzeitstudent.karafjavafx.main/src/test/java/com/github/teilzeitstudent/karafjavafx/main/Launcher.java
@@ -1,12 +1,41 @@
 package com.github.teilzeitstudent.karafjavafx.main;
 
-import javafx.application.Application;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.teilzeitstudent.karafjavafx.greetings.spi.GreetingsService;
 
 /**
  * Manually launch the {@link JavaxExample} application
  */
 public class Launcher {
-	public static void main(String[] args) {
-		Application.launch(JavaxExample.class, "");
+	private static final Logger LOGGER = LoggerFactory.getLogger(Launcher.class);
+	public static void main(String[] args) throws Exception {
+		List<GreetingsService> services = new ArrayList<GreetingsService>();
+		services.add(new GreetingsService() {
+			
+			@Override
+			public void printGreeting() {
+				LOGGER.info("Hi");
+				
+			}
+		});
+		services.add(new GreetingsService() {
+			
+			@Override
+			public void printGreeting() {
+				LOGGER.info("Hello");
+				
+			}
+		});
+		
+		JavaxExample example = new JavaxExample();
+		example.setGreetings(services);
+		
+		example.start((BundleContext)null);
 	}
 }
